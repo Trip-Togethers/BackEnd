7d43ed53562a38c90b6207bb4f9b1dfbadabbd7a
SeungChan <dlatmdcks60@gmail.com>
2025-01-30 13:11:10 +0900

    ♻️ Refactor: 지도 API 수정

--- src/controller/maps.controller.ts
+++ src/controller/maps.controller.ts
@@ -9,6 +9,9 @@ export const getMaps = async (req: Request<GetCalendarByIdParams>, res: Response
 };
 
 export const insertMaps = async (req: Request, res: Response) => {
+
+    if (!req.body.name || !req.body.address_name || !req.body.latitude || !req.body.longitude) return res.status(400).json({ message: "유효하지 않은 데이터입니다." });
+
     const map = await calendarServices.insertMaps(req.body);
     res.json(map);
 };
