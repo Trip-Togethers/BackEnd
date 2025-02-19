24959e1a2bd85e36bcfe05843fce734a1b9c4055
HaeRim <yjec14@naver.com>
2025-02-19 14:35:40 +0900

    🧪 Test: post_id 컬럼추가

--- src/entities/map.entity.ts
+++ src/entities/map.entity.ts
@@ -1,36 +1,50 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
 import { User } from './user.entity';
 
-@Entity('maps')
+@Entity("maps")
 export class Maps {
-    @PrimaryGeneratedColumn({ unsigned: true })
-    id: number;
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
 
-    @ManyToOne(() => User, (user) => user.id, { onDelete: 'CASCADE' })
-    @JoinColumn({ name: 'user_id' })
-    user: User;
-    
-    @Column({ type: 'int', unsigned: true })
-    userId: number;
+  @Column({ type: "varchar", length: 255, nullable: true })
+  place_id: string;
 
-    @Column({ type: 'varchar', length: 255, charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci' })
-    name: string;
+  @ManyToOne(() => User, (user) => user.id, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  user: User;
 
-    @Column({ type: 'float'})
-    latitude: number;
+  @Column({ type: "int", unsigned: true })
+  userId: number;
 
-    @Column({ type: 'float'})
-    longitude: number;
+  @Column({
+    type: "varchar",
+    length: 255,
+    charset: "utf8mb4",
+    collation: "utf8mb4_unicode_ci",
+  })
+  name: string;
 
-    @CreateDateColumn({ type: 'datetime' })
-    createdAt: Date;
+  @Column({ type: "float" })
+  latitude: number;
 
-    @Column({ type: 'float', nullable: true })
-    rating: number; // 평점
+  @Column({ type: "float" })
+  longitude: number;
 
-    @Column({ type: 'varchar', length: 255, nullable: true, charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci' })
-    vicinity: string; // 지역
+  @CreateDateColumn({ type: "datetime" })
+  createdAt: Date;
 
-    @Column('simple-array', { nullable: true })
-    photos: string[]; // 사진 URLs (배열로 저장)
+  @Column({ type: "float", nullable: true })
+  rating: number; // 평점
+
+  @Column({
+    type: "varchar",
+    length: 255,
+    nullable: true,
+    charset: "utf8mb4",
+    collation: "utf8mb4_unicode_ci",
+  })
+  vicinity: string; // 지역
+
+  @Column("simple-array", { nullable: true })
+  photos: string[]; // 사진 URLs (배열로 저장)
 }
